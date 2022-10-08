77c224a5fcf8875353b0b33c65eaa569d898fc12
MarvinJWendt <git@marvinjwendt.com>
2022-10-08 22:00:27 +0200

    init repo

--- doc.go
+++ doc.go
@@ -1,9 +1,5 @@
 /*
-Package template is used to generate new AtomicGo repositories.
-
-Write the description of the module here. You can use **markdown**!
-This description should clearly explain what the package does.
-
-Example description: https://golang.org/src/encoding/gob/doc.go
+Package stack is a simple implemntation of a stack data structure.
+It uses generics to make it type safe.
 */
-package template
+package stack
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module atomicgo.dev/template
+module atomicgo.dev/stack
 
 go 1.18
